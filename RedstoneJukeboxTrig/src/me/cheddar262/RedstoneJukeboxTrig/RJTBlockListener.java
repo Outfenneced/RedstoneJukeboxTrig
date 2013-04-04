bc7028bc1947fb0e42887ed05cba8eb0b9c40f62
cheddar262 <cheddar262@gmail.com>
2013-04-03 22:53:33 -0400

    Update to 2.5.0, reorganized code

--- RedstoneJukeboxTrig/src/me/cheddar262/RedstoneJukeboxTrig/RJTBlockListener.java
+++ RedstoneJukeboxTrig/src/me/cheddar262/RedstoneJukeboxTrig/RJTBlockListener.java
@@ -11,6 +11,7 @@ import org.bukkit.event.block.BlockRedstoneEvent;
 public class RJTBlockListener implements Listener{
 	public RedstoneJukeboxTrig plugin;
 	RJTPlayerListener PListener = new RJTPlayerListener(plugin);
+	private Material[] blockedCornerMaterials = {Material.REDSTONE_WIRE, Material.REDSTONE_TORCH_ON, Material.REDSTONE_TORCH_OFF, Material.WOOD_PLATE, Material.STONE_PLATE, Material.LEVER, Material.DIODE_BLOCK_OFF, Material.DIODE_BLOCK_ON};
 	
 	public RJTBlockListener (RedstoneJukeboxTrig instance) {
 		plugin = instance;
@@ -26,23 +27,23 @@ public class RJTBlockListener implements Listener{
 	private void checkPowerJukebox (Block block){
 		if( block.getType() == Material.JUKEBOX){
 			boolean redPower = redPowerChecker(block);
+			Jukebox jukebox = (Jukebox) block.getState();
+			String pos = jukebox.getWorld() + ", " + jukebox.getX() + ", " + jukebox.getY() + ", " + jukebox.getZ();
 			if(redPower && (!block.isBlockPowered())){
-				Jukebox jukebox = (Jukebox) block.getState();
-				String pos = jukebox.getWorld() + ", " + jukebox.getX() + ", " + jukebox.getY() + ", " + jukebox.getZ();
 				long StartTime = System.currentTimeMillis();
-				Material playing = getPlaying(jukebox);
+				Material playing = plugin.getPlaying(jukebox);
 				if(playing != Material.AIR){
-					if(RJTPlayerListener.posETime.containsKey(pos)){
-						long eTimeTwo = RJTPlayerListener.posETime.get(pos).longValue();
+					if(plugin.posETime.containsKey(pos)){
+						long eTimeTwo = plugin.posETime.get(pos).longValue();
 						if(eTimeTwo <= (System.currentTimeMillis())){
-							long EndTime = RJTPlayerListener.findEndTime(StartTime, playing);
-							RJTPlayerListener.posETime.put(pos, EndTime);
+							long EndTime = plugin.findEndTime(StartTime, playing);
+							plugin.posETime.put(pos, EndTime);
 							jukebox.setPlaying(playing);
 						}
 					}
 					else{
-						long EndTime = RJTPlayerListener.findEndTime(StartTime, playing);
-						RJTPlayerListener.posETime.put(pos, EndTime);
+						long EndTime = plugin.findEndTime(StartTime, playing);
+						plugin.posETime.put(pos, EndTime);
 						jukebox.setPlaying(playing);
 					}
 				}
@@ -50,38 +51,15 @@ public class RJTBlockListener implements Listener{
 		}
 	}
 	private boolean redPowerChecker(Block block) {
-		Material BlockNorthEastType = block.getRelative(BlockFace.NORTH_EAST).getType();
-		Material BlockNorthWestType = block.getRelative(BlockFace.NORTH_WEST).getType();
-		Material BlockSouthEastType = block.getRelative(BlockFace.SOUTH_EAST).getType();
-		Material BlockSouthWestType = block.getRelative(BlockFace.SOUTH_WEST).getType();
-		boolean NE = false;
-		boolean NW = false;
-		boolean SE = false;
-		boolean SW = false;
-		if((BlockNorthEastType != Material.REDSTONE_WIRE) && (BlockNorthEastType != Material.REDSTONE_TORCH_ON) && (BlockNorthEastType != Material.REDSTONE_TORCH_OFF) && (BlockNorthEastType != Material.WOOD_PLATE) && (BlockNorthEastType != Material.STONE_PLATE) && (BlockNorthEastType != Material.LEVER) && (BlockNorthEastType != Material.DIODE_BLOCK_OFF) && (BlockNorthEastType != Material.DIODE_BLOCK_ON)) {
-			NE = true;
+		Material BlockNorthEast = block.getRelative(BlockFace.NORTH_EAST).getType();
+		Material BlockNorthWest = block.getRelative(BlockFace.NORTH_WEST).getType();
+		Material BlockSouthEast = block.getRelative(BlockFace.SOUTH_EAST).getType();
+		Material BlockSouthWest = block.getRelative(BlockFace.SOUTH_WEST).getType();
+		for (Material blockedMaterial: blockedCornerMaterials){
+			if(BlockNorthEast == blockedMaterial || BlockNorthWest == blockedMaterial || BlockSouthEast == blockedMaterial || BlockSouthWest == blockedMaterial){
+				return false;
+			}
 		}
-		if((BlockNorthWestType != Material.REDSTONE_WIRE) && (BlockNorthWestType != Material.REDSTONE_TORCH_ON) && (BlockNorthWestType != Material.REDSTONE_TORCH_OFF) && (BlockNorthWestType != Material.WOOD_PLATE) && (BlockNorthWestType != Material.STONE_PLATE) && (BlockNorthWestType != Material.LEVER) && (BlockNorthWestType != Material.DIODE_BLOCK_OFF) && (BlockNorthWestType != Material.DIODE_BLOCK_ON)) {
-			NW = true;
-		}
-		if((BlockSouthEastType != Material.REDSTONE_WIRE) && (BlockSouthEastType != Material.REDSTONE_TORCH_ON) && (BlockSouthEastType != Material.REDSTONE_TORCH_OFF) && (BlockSouthEastType != Material.WOOD_PLATE) && (BlockSouthEastType != Material.STONE_PLATE) && (BlockSouthEastType != Material.LEVER) && (BlockSouthEastType != Material.DIODE_BLOCK_OFF) && (BlockSouthEastType != Material.DIODE_BLOCK_ON)) {
-			SE = true;
-		}
-		if((BlockSouthWestType != Material.REDSTONE_WIRE) && (BlockSouthWestType != Material.REDSTONE_TORCH_ON) && (BlockSouthWestType != Material.REDSTONE_TORCH_OFF) && (BlockSouthWestType != Material.WOOD_PLATE) && (BlockSouthWestType != Material.STONE_PLATE) && (BlockSouthWestType != Material.LEVER) && (BlockSouthWestType != Material.DIODE_BLOCK_OFF) && (BlockSouthWestType != Material.DIODE_BLOCK_ON)) {
-			SW = true;
-		}
-		if(NE && NW && SE && SW){
-			return true;
-		} else{
-			return false;
-		}
-	}
-	public Material getPlaying(Jukebox jb) {
-	    try {
-	        jb.getPlaying();
-	        return jb.getPlaying();
-	    } catch (Exception e) {
-	        return Material.AIR;
-	    }
+		return true;
 	}
 }
--- RedstoneJukeboxTrig/src/me/cheddar262/RedstoneJukeboxTrig/RedstoneJukeboxTrig.java
+++ RedstoneJukeboxTrig/src/me/cheddar262/RedstoneJukeboxTrig/RedstoneJukeboxTrig.java
@@ -1,7 +1,12 @@
 package me.cheddar262.RedstoneJukeboxTrig;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import org.bukkit.Material;
+import org.bukkit.block.Jukebox;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -10,6 +15,21 @@ public class RedstoneJukeboxTrig extends JavaPlugin{
 	Logger log = Logger.getLogger("Minecraft");
 	private final RJTBlockListener blockListener = new RJTBlockListener(this);
 	private final RJTPlayerListener playerListener = new RJTPlayerListener(this);
+	public Map<String, Long> posETime = Collections.synchronizedMap(new HashMap<String, Long> ());
+	static long[] recordTimes = { //(Minutes * 60) + seconds
+			((3*60)+3), //13
+			((3*60)+6), //cat
+			((5*60)+12), //blocks
+			((3*60)+5), //chirp
+			((2*60)+54), //far
+			((3*60)+17), //mall
+			((1*60)+37), //mellohi
+			((2*60)+30), //stal
+			((3*60)+9), //strad
+			((4*60)+10), //ward
+			((1*60)+10), //11
+			((3*60)+52)}; //wait
+	
 	public void onEnable() {
 		PluginManager pm = getServer().getPluginManager();
 		
@@ -21,4 +41,63 @@ public class RedstoneJukeboxTrig extends JavaPlugin{
 		log.info("RedstoneJukeboxTrig has been disabled.");
 		
 	}	
+	
+	//public Map<String, Long> hashIt(String pos, long endTime) {
+		//posETime.put(pos, endTime);
+		//return posETime;
+	//}
+
+	public long findEndTime(long stTime, Material record) {
+		long end = 0;
+		long timeAdd = 0;
+		if(record == Material.GOLD_RECORD){ //13
+			timeAdd = (recordTimes[0]);
+		}
+		else if(record == Material.GREEN_RECORD){ //cat
+			timeAdd = (recordTimes[1]);
+		}
+		else if(record == Material.RECORD_3){ //blocks
+			timeAdd = (recordTimes[2]);
+		}
+		else if(record == Material.RECORD_4){ //chirp
+			timeAdd = (recordTimes[3]);
+		}
+		else if(record == Material.RECORD_5){ //far
+			timeAdd = (recordTimes[4]);
+		}
+		else if(record == Material.RECORD_6){ //mall
+			timeAdd = (recordTimes[5]);
+		}
+		else if(record == Material.RECORD_7){ //mellohi
+			timeAdd = (recordTimes[6]);
+		}
+		else if(record == Material.RECORD_8){ //stal
+			timeAdd = (recordTimes[7]);
+		}
+		else if(record == Material.RECORD_9){ //strad
+			timeAdd = (recordTimes[8]);
+		}
+		else if(record == Material.RECORD_10){ //ward
+			timeAdd = (recordTimes[9]);
+		}
+		else if(record == Material.RECORD_11){ //11
+			timeAdd = (recordTimes[10]);
+		}
+		else if(record == Material.RECORD_12){ //11
+			timeAdd = (recordTimes[11]);
+		}
+		
+		timeAdd = timeAdd*1000;
+		end = stTime + timeAdd;
+		return end;
+	}
+	
+	public Material getPlaying(Jukebox jb) {
+	    try {
+	        jb.getPlaying();
+	        return jb.getPlaying();
+	    } catch (Exception e) {
+	        return Material.AIR;
+	    }
+	}
 }
